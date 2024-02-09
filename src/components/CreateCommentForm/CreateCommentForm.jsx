fc7357a69fe6c189ded46276e1661c67f88ca28b
Viktor Khasenko <developer@viktorkhasenko.com>
2024-02-09 12:53:42 +0000

    Add loading and error handling to CreateCommentForm
    Added a loading state to provide feedback to the user while the comment submission is processing. Also implemented an error handling system to alert the user if a comment fails to publish. Optimized loading and error display within the form component.

--- src/components/CreateCommentForm/CreateCommentForm.jsx
+++ src/components/CreateCommentForm/CreateCommentForm.jsx
@@ -3,44 +3,56 @@ import './CreateCommentForm.scss';
 import { createComment } from '../../utils/utils.js';
 import { useSelector } from 'react-redux';
 import Authorisation from '../Authorisation/Authorisation.jsx';
+import Loader from '../Loader/Loader.jsx';
 
 const CreateCommentForm = ({ articleId, comments, setComments }) => {
 
   const [commentBody, setCommentBody] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorAlert, setErrorAlert] = useState(null);
   const user = useSelector((state) => state.user.user);
 
   const formHandle = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     createComment(articleId, user.username, commentBody).then((response) => {
-      setComments([...comments, response]);
-      setCommentBody('');
+      if (response) {
+        setComments([...comments, response]);
+        setCommentBody('');
+      } else {
+        setErrorAlert('Error: Comment not published');
+      }
+      setIsLoading(false);
     });
   };
 
-  return ( user ? (
-        <form className="form-block" onSubmit={formHandle}>
-          <div className="row">
-            <div className="col-12 mb-4">
-              <div className="form-group">
-                <label className="comment-label mb-2 fs-4" htmlFor="comment">Comment</label>
-                <textarea
-                  id="comment"
-                  className="form-input"
-                  placeholder="Your text"
-                  value={commentBody}
-                  onChange={({ target }) => {
-                    setCommentBody(target.value);
-                  }}
-                  required
-                ></textarea>
-              </div>
+  return (user ? (
+      <form className="form-block" onSubmit={formHandle}>
+        <div className="row">
+          <div className="col-12 mb-4">
+            <div className="form-group">
+              <label className="comment-label mb-2 fs-4" htmlFor="comment">Comment</label>
+              <textarea
+                id="comment"
+                className="form-input"
+                placeholder="Your text"
+                value={commentBody}
+                onChange={({ target }) => {
+                  setCommentBody(target.value);
+                }}
+                required
+              ></textarea>
             </div>
-            <button className="btn btn-form">Comment</button>
           </div>
-        </form>
-      ) : (
-        <Authorisation/>
-      )
+          {isLoading ? (<Loader/>) : (<button className="btn btn-form">Comment</button>)}
+          {errorAlert && (
+            <div className="alert alert-danger" role="alert">{errorAlert}</div>
+          )}
+        </div>
+      </form>
+    ) : (
+      <Authorisation/>
+    )
   );
 };
 
